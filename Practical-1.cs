971be6e09b6e1a08aed6d07c280f52fa16968a50
S-Git-Coder <sahilpatel9313@gmail.com>
2024-10-07 13:47:06 +0530

    Practical_1

---
+++ Practical-1.cs
@@ -0,0 +1,11 @@
+// Write a C# Sharp program to print Hello and your name in a separate line.
+
+public class Practical_1
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello");
+        Console.WriteLine("Sahil");
+    }
+}
