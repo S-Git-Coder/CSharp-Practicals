8c6588ba8249612181c024d714b8296f3a786da0
S-Git-Coder <sahilpatel9313@gmail.com>
2024-10-07 13:57:30 +0530

    Practical_3

---
+++ Practical_3.cs
@@ -0,0 +1,14 @@
+// Write a C# Sharp program to print the result of dividing two numbers.
+
+public class Practical_3
+{
+    public static void Main(string[] args)
+    {
+        //Console.WriteLine(36 / 6);
+
+        int No1 = 36;
+        int No2 = 6;
+
+        Console.WriteLine(No1 / No2);
+    }
+}
