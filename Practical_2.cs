fe1764ce9fc1952e16c828986e999519a1bc6019
S-Git-Coder <sahilpatel9313@gmail.com>
2024-10-07 13:53:48 +0530

    Practical_2

---
+++ Practical_2.cs
@@ -0,0 +1,14 @@
+// Write a C# Sharp program to print the sum of two numbers.
+
+public class Practical_2
+{
+    public static void Main(string[] args)
+    {
+        //Console.WriteLine(10 + 10);
+
+        int No1 = 10;
+        int No2 = 10;
+
+        Console.WriteLine(No1 + No2);
+    }
+}
